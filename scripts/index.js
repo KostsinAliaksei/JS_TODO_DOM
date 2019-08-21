cf5d3b76f4d0a0b253d73de8b294981728b4d827
Aliaksei Kostsin <admin_04@inautomarket.com>
2019-08-21 16:16:39 +0300

    Added Component class and extented class

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,50 @@ class Element {
         this.children = children;
     }
 }
+
+//==================КЛАСС КОМПОНЕНТА (ОТРИСОВКА)=======================
+class Component {
+    constructor(model) {    //===========Проверка на инстант класса Element======
+        if (model instanceof Element) {
+            this.model = model;
+        } else {
+            console.error('Error! Model is not an Element object');
+        }
+    }
+
+    render() {}
+
+}
+
+//===========Дочерний класс для отрисовки любых компонентов=============
+class TodoComponent extends Component{
+
+    render() {
+
+        const type = this.model.type;
+        const props = this.model.props;
+        const children = this.model.children;
+        const attributes = this.model.attributes;
+
+        let element;
+
+        if (typeof type === 'string') {
+            element = document.createElement(type);
+        }
+
+        for (let attrib in attributes) {
+            element[attrib] = attributes[attrib];
+        }
+
+        if (typeof children === 'string') {
+            element.innerText = children;
+        } else if (children === null) {
+            return null;
+        } else if (typeof children === 'object') {
+            const liComponents = children.map(elem => new TodoComponent(elem));
+            liComponents.forEach(elem => element.appendChild(elem.render()));
+        }
+
+        return element;
+    }
+}
