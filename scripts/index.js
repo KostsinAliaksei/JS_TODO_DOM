63b1be31cb34284cf00ab7e8e174e159be1b8c32
Aliaksei Kostsin <admin_04@inautomarket.com>
2019-08-21 16:15:46 +0300

    Added Element class

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
 //================Срисок атрибутов для элементов=============
 const attrFields = ['id', 'htmlFor', 'className', 'checked', 'type',
     'textContent', 'innerText', 'placeholder', 'onclick'];
+
+//==================КЛАСС ЭЛЕМЕНТА==================
+class Element {
+    constructor(type, props, children) {
+
+        this.type = type;
+        this.props = {};
+
+        //===Сортировка (атрибут / мусор)======
+        let arr = {};
+        for (let attr_el in props) {
+            if (attrFields.indexOf(attr_el) !== -1) {
+                arr[attr_el] = props[attr_el];
+            } else {
+                this.props[attr_el] = props[attr_el];
+            }
+        }
+        //=========================================
+
+        this.attributes = arr;
+        this.children = children;
+    }
+}
